d766b2a33ad2c2ac46271e676612a356170c3f1d
July-4n <september4n@gmail.com>
2023-09-08 10:51:18 +0500

    init: start lesson2

---
+++ lesson_01/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const goodsName = 'player';
+const goodsQuantity = '2';
+const goodsCategory = 'tv, audio';
+const goodsPrice = '3099';
+const total = goodsQuantity * goodsPrice;
+
+console.log(goodsName);
+console.log(total);
