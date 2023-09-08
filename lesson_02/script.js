1612545a795cddf2b596dd657e41fdf95c0aaa82
July-4n <september4n@gmail.com>
2023-09-08 11:29:57 +0500

    complete task2

---
+++ lesson_02/script.js
@@ -0,0 +1,16 @@
+'use strict';
+{
+  const goodsName = 'player';
+  const goodsQuantity = 2;
+  const goodsCategory = 'tv, audio';
+  const goodsPrice = 3099;
+
+  console.log(goodsName);
+  console.log(`общая сумма: ${goodsQuantity * goodsPrice}`);
+}
+{
+  const goodsName = 'gaming-chair';
+  const goodsQuantity = 1;
+  const goodsCategory = 'furniture';
+  const goodsPrice = 23056;
+}
