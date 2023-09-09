e8f63eca87799eef59aa319b34e5752e053f36d2
July-4n <september4n@gmail.com>
2023-09-09 20:35:50 +0500

    solve task4-4

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -11,11 +11,22 @@
 }
 
 {
-  const getMathMarks = +prompt("Введите кол-во баллов по математике");
-  const getLanguageMarks = +prompt("Введите кол-во баллов по русскому языку");
-  const getComputerScienceMarks = +prompt("Введите кол-во баллов по информатике");
+  const getMathMarks = +prompt('Введите кол-во баллов по математике');
+  const getLanguageMarks = +prompt('Введите кол-во баллов по русскому языку');
+  const getComputerScienceMarks = +prompt('Введите кол-во баллов по информатике');
 
   if ((getMathMarks + getLanguageMarks + getComputerScienceMarks) >= 265) {
-    alert("Поздравляю, вы поступили на бюджет!");
+    alert('Поздравляю, вы поступили на бюджет!');
+  }
+}
+
+{
+  const minAmount = 100;
+  const getMoney = +prompt('Какую сумму вы хотите снять?');
+
+  if (getMoney >= 100) {
+    console.log(`Заберите ${getMoney}`);
+  } else {
+    console.log(`Сумма не может быть меньше ${minAmount} рублей`);
   }
 }
