bf23b5865b3035ff7d1faa8642785bfd265a55fe
July-4n <september4n@gmail.com>
2023-09-09 19:36:03 +0500

    solve task4-3

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -1,10 +1,21 @@
 'use strict';
+{
+  const rain = Math.round(Math.random());
 
-const rain = Math.round(Math.random());
+  if (rain == 1) {
+    'Пошёл дождь. Возьмите зонт!'
+  };
+  if (rain == 0) {
+    'Дождя нет!'
+  };
+}
 
-if (rain == 1) {
-  'Пошёл дождь. Возьмите зонт!'
-};
-if (rain == 0) {
-  'Дождя нет!'
-};
+{
+  const getMathMarks = +prompt("Введите кол-во баллов по математике");
+  const getLanguageMarks = +prompt("Введите кол-во баллов по русскому языку");
+  const getComputerScienceMarks = +prompt("Введите кол-во баллов по информатике");
+
+  if ((getMathMarks + getLanguageMarks + getComputerScienceMarks) >= 265) {
+    alert("Поздравляю, вы поступили на бюджет!");
+  }
+}
