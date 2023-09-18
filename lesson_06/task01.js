266f62c622f67737a484e4def6e10888728cd1d8
July-4n <september4n@gmail.com>
2023-09-18 14:24:59 +0500

    solve task6

---
+++ lesson_06/task01.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let i = 1; i < 11; i++) {
+  for (let k = 1; k < 11; k++) {
+    let j= `${i**k}`;
+    console.log(j);
+  }
+}
---
+++ lesson_06/task02.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const isPrime = (num) => {
+  for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
