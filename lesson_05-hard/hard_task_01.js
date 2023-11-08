e7e0b9e8946a43427b2ecffa246eec88c20e0e50
July-4n <september4n@gmail.com>
2023-11-08 23:28:32 +0500

    solve lesson_05-hard

--- lesson_05-hard/hard_task_01.js
+++ lesson_05-hard/hard_task_01.js
@@ -1,1 +1,11 @@
 'use strict';
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+};
+
+console.log(`Наибольший общий делитель 100 и 6550 равен ${gcd(100, 6550)}`);
+console.log(`Наибольший общий делитель 244 и 158 равен ${gcd(244, 158)}`);
--- lesson_05-hard/hard_task_02.js
+++ lesson_05-hard/hard_task_02.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const getMinNumber = () => {
+  const numberOne = +prompt('Введите число');
+  const numberTwo = +prompt('Введите число');
+
+  const diff = numberOne - numberTwo;
+  return diff > 0 ? numberTwo : numberOne;
+};
+
+console.log(`Минимальное значение равно ${getMinNumber()}`);
